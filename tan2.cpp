51921ab6dc2a98971be0d453b7606c8ff967007c
tanmoyPaul12 <tanmoy01paul@gmail.com>
2025-09-03 00:58:55 +0530

    functions loops

--- tan2.cpp
+++ tan2.cpp
@@ -309,24 +309,33 @@
 //     return 0;
 // }
 
-#include<iostream>
-#include<string>
+// #include<iostream>
+// #include<string>
 
-using namespace std;
+// using namespace std;
 
-int main(){
-    string response;
+// int main(){
+//     string response;
 
-    while (true)
-    {
-        cout<<"Do you want more tea (type 'stop' to exit)?:";
-        getline(cin,response);
+//     while (true)
+//     {
+//         cout<<"Do you want more tea (type 'stop' to exit)?:";
+//         getline(cin,response);
         
-        if(response=="stop"){
-            break;
-        }
-        cout<<"Here is your another cup of tea\n"<<endl;
-    }    
-    cout<<"No more tea will be served";
-    return 0;
-}
+//         if(response=="stop"){
+//             break;
+//         }
+//         cout<<"Here is your another cup of tea\n"<<endl;
+//     }    
+//     cout<<"No more tea will be served";
+//     return 0;
+// }
+
+
+
+
+// Functions
+
+
+// Oops
+
